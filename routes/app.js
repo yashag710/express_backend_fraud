603dbecdef0884fa2467f9719284f00a1e05a41e
YASH AGARWAL <143899665+yashag710@users.noreply.github.com>
2025-04-30 05:05:00 +0530

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -1,61 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");  
-// const { loginUser, registerUser } = require('../controllers/authController');
-// const { isLoggedIn } = require("../middlewares/isLoggedIn");
-// const userModel = require("../models/userModel");
-// const multer = require("multer");
-// const dotenv = require("dotenv");
-// const {geminiController} = require("../controllers/geminiController");
-const {checkFraud} = require("../express-backend/controllers/ruleBasedController"); 
-const transactionController = require('../express-backend/controllers/transactionController');
-const { updateController } = require("../express-backend/controllers/updateController");
-const reportingController = require("../express-backend/controllers/reportingController");
-// dotenv.config();
-// const upload = multer();  
-// const upload = require("../middlewares/multer");
-// const dotenv = require("dotenv");
-// const {geminiController} = require("../controllers/geminiController");
 
-// dotenv.config();
-
-// let analyzeCareer;
-// // Dynamic import of the ES module
-// import('../ai_path.mjs').then(module => {
-//     analyzeCareer = module.analyzeCareer;
-// }).catch(err => {
-//     console.error('Error importing ai_path.mjs:', err);
-// });
-
-// // Login Routes
-// router.post("/login", loginUser);
-
-// // Signup Route
-// router.post("/signup", registerUser);
-
-// router.post("/upload", upload.single("image"), (req, res) => {
-//   if (!req.file) {
-//     console.log("File not received");
-//     return res.status(400).json({ error: "No file uploaded" });
-//   }
-//   console.log("File uploaded");
-//   res.json({ imageUrl: req.file.path }); // Cloudinary URL
-// });
-
+const {checkFraud} = require("../controllers/ruleBasedController"); 
+const transactionController = require('../controllers/transactionController');
+const { updateController } = require("../controllers/updateController");
+const reportingController = require("../controllers/reportingController");
 
 router.post("/ruleBased", checkFraud);
-// router.post("/upload", upload.single("image"), (req, res) => {
-// //   if (!req.file) {
-// //     console.log("File not received");
-// //     return res.status(400).json({ error: "No file uploaded" });
-// //   }
-// //   console.log("File uploaded successfully");
-// //   res.json({ 
-// //     success: true,
-// //     imageUrl: req.file.path,
-// //     message: "File uploaded successfully"
-// //   });
-// // });
 
 router.post("/update" , updateController);
 
