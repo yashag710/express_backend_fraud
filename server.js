710af430e5c06045f34e07795e88aea9a6fa808d
YASH AGARWAL <143899665+yashag710@users.noreply.github.com>
2025-05-01 14:37:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-const router = express.Router();
-const app = express();
-const PORT = process.env.PORT || 5000;
 const cookieParser = require("cookie-parser");
-const transactionRoutes = require("./routes/transactionRoutes");
-
-require("dotenv").config();
-app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173',  // Your frontend URL specifically
-    credentials: true
-  }));
-app.use(cookieParser());
-
-const {dbConnect} = require("./config/mongoose-connection");
-
-dbConnect();
-
+const { dbConnect } = require("./config/mongoose-connection");
 const appRoutes = require("./routes/app");
 
-app.use("/api", appRoutes)
-app.use("/api/transaction", transactionRoutes);
-// start the server
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load environment variables
+require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(cookieParser());
+
+// Database connection
+dbConnect();
+
+// Routes
+app.use("/api", appRoutes);
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
