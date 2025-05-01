bdff93dc2db6c2233dcdfc6afc3aa48c4ddb6930
YASH AGARWAL <143899665+yashag710@users.noreply.github.com>
2025-05-01 14:37:37 +0530

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");  
 
-const {checkFraud} = require("../controllers/ruleBasedController"); 
+// Import controllers
+const { checkFraud } = require("../controllers/ruleBasedController");
 const transactionController = require('../controllers/transactionController');
 const { updateController } = require("../controllers/updateController");
 const reportingController = require("../controllers/reportingController");
 
+// Define routes
 router.post("/ruleBased", checkFraud);
-
-router.post("/update" , updateController);
-
+router.post("/update", updateController);
 router.get('/transactions', transactionController.getTransactions);
-
 router.get('/transactions/stats', transactionController.getTransactionStats);
-
 router.post('/result', reportingController.reportFraud);
 
 module.exports = router;
